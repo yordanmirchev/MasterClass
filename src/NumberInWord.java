81663deebaccb3512aebeabbc5e05fe60036ae15
Yordan Mirchev (Mac) <yordan.mirchev@tis.biz>
2022-12-20 10:19:39 +0200

    Conditions. Part1.

---
+++ src/NumberInWord.java
@@ -0,0 +1,30 @@
+public class NumberInWord {
+
+    public static void main(String[] args) {
+
+
+        for (int i = -1; i < 11; i++) {
+            printNumberInWord(i);
+        }
+    }
+
+    public static void printNumberInWord(int num) {
+        String word =
+                switch (num) {
+                    case 0 -> "ZERO";
+                    case 1 -> "ONE";
+                    case 2 -> "TWO";
+                    case 3 -> "THREE";
+                    case 4 -> "FOUR";
+                    case 5 -> "FIVE";
+                    case 6 -> "SIX";
+                    case 7 -> "SEVEN";
+                    case 8 -> "EIGHT";
+                    case 9 -> "NINE";
+                    default -> "other";
+
+                };
+
+        System.out.println(word);
+    }
+}
