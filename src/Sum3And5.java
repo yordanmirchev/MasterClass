92d2bed4f89b4b93209ceba2bd0ec95dd9cf4408
yordanmirchev <65480426+yordanmirchev@users.noreply.github.com>
2022-12-21 14:28:48 +0200

    For loop. Basic.

---
+++ src/Sum3And5.java
@@ -0,0 +1,19 @@
+public class Sum3And5 {
+
+    public static void main(String[] args) {
+
+        int total_sum=0;
+        int total_count=0;
+
+        for (int i = 1 ; i <=1000  && total_count < 5; i++){
+            if (i%3 ==0 && i %5 ==0){
+                System.out.println("Number " + i + " is divisible by 3 and 5.");
+
+                total_sum += i;
+                total_count++;
+            }
+        }
+
+        System.out.println("\nTotal sum of the numbers: " + total_sum);
+    }
+}
