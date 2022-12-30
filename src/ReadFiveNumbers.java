057870fa708d5e276b0400ade4ac3f1e217133bd
Yordan Mirchev (Mac) <yordan.mirchev@tis.biz>
2022-12-30 09:37:02 +0200

    Inputs and overload.

---
+++ src/ReadFiveNumbers.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ReadFiveNumbers {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i < 6; i++) {
+            int number = 0;
+            do {
+                number = readInt(i);
+            }
+            while (number < 0);
+            sum += number;
+        }
+
+        System.out.println("The total is: " + sum);
+
+    }
+
+    public static int readInt(int n) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter number #" + n + " :");
+        int result = 0;
+        try {
+            result = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid number.");
+            return -1;
+        }
+        return result;
+    }
+}
